ce881ee07553b17ab3afc4fa95507ead94aa3f80
Mark Skilbeck <mark.skilbeck@rigetti.com>
2019-05-20 11:04:28 -0700

    doc

--- app/src/utils.lisp
+++ app/src/utils.lisp
@@ -1,7 +1,9 @@
 (in-package :quilc)
 
 (defun clamp-syslog-name (name)
-  ""
+  "Produces a name like \"<A>...<B>\" where <A> is a 15-char chunk
+from the beginning of the name, and <B> is a 30-char chunk from the
+end of the name."
   (let ((len (length name)))
     (if (<= len 48)
         name
